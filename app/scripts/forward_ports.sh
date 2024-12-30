7c64261d5257ae659e6e6d13b9e23f91d6eb5a3a
huji <389491@niuitmo.ru>
2024-12-30 19:23:35 +0300

    refactor: minor fixes for scripts

--- app/scripts/forward_ports.sh
+++ app/scripts/forward_ports.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sshpass -f "/home/huji/.helios_pass" ssh -p 2222 -L 8080:localhost:29043 s389491@helios.cs.ifmo.ru
+sshpass -f "/home/huji/.helios_pass" ssh -p 2222 -N -L 8080:localhost:29043 s389491@helios.cs.ifmo.ru
